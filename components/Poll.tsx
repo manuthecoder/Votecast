ac547d1d07e1dcaaa97cb34f70dcdc20acffaba4
Manu <manuthecoder@protonmail.com>
2022-07-07 09:33:42 +0000

    E

--- components/Poll.tsx
+++ components/Poll.tsx
@@ -1,7 +1,6 @@
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import Card from "@mui/material/Card";
-import AppBar from "@mui/material/AppBar";
 import Grid from "@mui/material/Grid";
 import TextField from "@mui/material/TextField";
 import { Option } from "./Option";
@@ -9,13 +8,83 @@ import React from "react";
 import InputAdornment from "@mui/material/InputAdornment";
 import CircularProgress from "@mui/material/CircularProgress";
 import IconButton from "@mui/material/IconButton";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+
+interface TabPanelProps {
+  children?: React.ReactNode;
+  index: number;
+  value: number;
+}
+
+function Chat() {
+  return (
+    <>
+      <Box
+        sx={{
+          height: "calc(100vh - 330px)",
+        }}
+      ></Box>
+      <Box sx={{ width: "100%" }}>
+        <TextField
+          fullWidth
+          placeholder="Type message here"
+          InputProps={{
+            sx: {
+              borderRadius: "0 0 19px 19px",
+            },
+            endAdornment: (
+              <InputAdornment
+                position="end"
+                sx={{
+                  mr: 1,
+                  "& span": {
+                    transition: "all .2s cubic-bezier(.17,.67,.44,1.31)",
+                  },
+                  "&:hover span": { transform: "rotate(-90deg)" },
+                  "&:active span": { transform: "rotate(-90deg)" },
+                }}
+              >
+                <IconButton aria-label="toggle password visibility" edge="end">
+                  <span className="material-symbols-rounded">east</span>
+                </IconButton>
+              </InputAdornment>
+            ),
+          }}
+        />
+      </Box>
+    </>
+  );
+}
+
+function TabPanel(props: TabPanelProps) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && <Box sx={{ py: 2 }}>{children}</Box>}
+    </div>
+  );
+}
 
 function PollCard({ data }) {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+  };
+
   const [voted, setVoted] = React.useState<boolean>(false);
   return (
     <>
-      <Grid container spacing={2}>
-        <Grid item xs={voted ? 4 : 5}>
+      <Grid container sx={{ width: "100%" }} spacing={4}>
+        <Grid item xs={5}>
           <Card
             sx={{
               maxHeight: "calc(100vh - 200px)",
@@ -27,24 +96,49 @@ function PollCard({ data }) {
             }}
             variant="outlined"
           >
-            <img
-              src={data.banner}
-              style={{
-                width: "100%",
-                borderRadius: "28px",
-                height: "200px",
-                objectFit: "cover",
+            <Tabs
+              value={value}
+              variant="fullWidth"
+              onChange={handleChange}
+              aria-label="basic tabs example"
+              sx={{
+                "& .MuiTabs-indicator": {
+                  height: "100%",
+                  opacity: 0.4,
+                  borderRadius: 5,
+                },
+                "& .MuiButtonBase-root": {
+                  textTransform: "none",
+                  color: "#000!important",
+                },
               }}
-            />
-            <Typography variant="h3" sx={{ mb: 2, fontWeight: "900", mt: 2 }}>
-              {data.name}
-            </Typography>
-            <Typography variant="h6" sx={{ mb: 2, mt: 2 }}>
-              {data.description}
-            </Typography>
+            >
+              <Tab sx={{ borderRadius: 5 }} label="Instructions" />
+              <Tab sx={{ borderRadius: 5 }} label="Chat" />
+            </Tabs>
+            <TabPanel value={value} index={0}>
+              <img
+                src={data.banner}
+                style={{
+                  width: "100%",
+                  borderRadius: "28px",
+                  height: "200px",
+                  objectFit: "cover",
+                }}
+              />
+              <Typography variant="h3" sx={{ mb: 2, fontWeight: "900", mt: 2 }}>
+                {data.name}
+              </Typography>
+              <Typography variant="h6" sx={{ mb: 2, mt: 2 }}>
+                {data.description}
+              </Typography>
+            </TabPanel>
+            <TabPanel value={value} index={1}>
+              <Chat />
+            </TabPanel>
           </Card>
         </Grid>
-        <Grid item xs={voted ? 5 : 7}>
+        <Grid item xs={7}>
           <Card
             sx={{
               display: "flex",
@@ -56,7 +150,6 @@ function PollCard({ data }) {
               height: "100%",
               overflow: "auto",
               p: 3,
-              border: 0,
             }}
             variant="outlined"
           >
@@ -78,79 +171,6 @@ function PollCard({ data }) {
             </Box>
           </Card>
         </Grid>
-        {voted && (
-          <Grid item xs={3}>
-            <Card
-              sx={{
-                borderRadius: 5,
-                maxHeight: "calc(100vh - 200px)",
-                mt: 2,
-                height: "100%",
-                overflow: "auto",
-                p: 3,
-                position: "relative",
-              }}
-              variant="outlined"
-            >
-              <AppBar
-                position="absolute"
-                sx={{
-                  top: 0,
-                  left: 0,
-                  borderBottom: "1px solid #ddd",
-                  background: "#efefef",
-                  display: "flex",
-                  justifyContent: "center",
-                  px: 2,
-                  height: "50px!important",
-                }}
-                elevation={0}
-              >
-                <Typography>Chat</Typography>
-              </AppBar>
-              <Box></Box>
-              <Box
-                sx={{
-                  position: "absolute",
-                  bottom: 0,
-                  left: 0,
-                  width: "100%",
-                }}
-              >
-                <TextField
-                  fullWidth
-                  placeholder="Type message here"
-                  InputProps={{
-                    sx: {
-                      borderRadius: "0 0 19px 19px",
-                    },
-                    endAdornment: (
-                      <InputAdornment
-                        position="end"
-                        sx={{
-                          mr: 1,
-                          "& span": {
-                            transition:
-                              "all .2s cubic-bezier(.17,.67,.44,1.31)",
-                          },
-                          "&:hover span": { transform: "rotate(-90deg)" },
-                          "&:active span": { transform: "rotate(-90deg)" },
-                        }}
-                      >
-                        <IconButton
-                          aria-label="toggle password visibility"
-                          edge="end"
-                        >
-                          <span className="material-symbols-rounded">east</span>
-                        </IconButton>
-                      </InputAdornment>
-                    ),
-                  }}
-                />
-              </Box>
-            </Card>
-          </Grid>
-        )}
       </Grid>
     </>
   );
@@ -171,7 +191,7 @@ export function Poll({ data }: any) {
         justifyContent: "center",
       }}
     >
-      <Box sx={{ mt: 4, px: 3 }}>
+      <Box sx={{ mt: 4, px: 3, width: "100vw" }}>
         {isLoading ? <CircularProgress /> : <PollCard data={data} />}
       </Box>
     </Box>
